f013da7c40d45e9d934ee53867e36c02474273f9
Alberto <25537372+albertobas@users.noreply.github.com>
2022-03-08 00:54:33 +0100

    Added the contract provider

--- react-app/src/index.tsx
+++ react-app/src/index.tsx
@@ -2,11 +2,14 @@ import * as React from 'react';
 import ReactDOM from 'react-dom';
 import App from 'App';
 import { ChakraProvider, theme } from '@chakra-ui/react';
+import ContractProvider from 'utils/context/ContractContext';
 
 ReactDOM.render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
-      <App />
+      <ContractProvider>
+        <App />
+      </ContractProvider>
     </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('root')
