46227544aac48fcaf76e7de76f9e8be1650ce9f8
Alberto <25537372+albertobas@users.noreply.github.com>
2022-03-07 19:57:40 +0100

    Share files script

--- hardhat/scripts/shareFiles.ts
+++ hardhat/scripts/shareFiles.ts
@@ -4,7 +4,7 @@ import { join } from 'path';
 
 async function shareFiles() {
   const hardhatContractsPath = join(__dirname, '../artifacts/contracts');
-  const hardhatContractsTypePath = join(__dirname, '../typechain');
+  const hardhatContractsTypePath = join(__dirname, '../typechain-types');
   const reactContractsPath = join(__dirname, '../../react-app/src/utils/contracts');
   const reactContractsTypesPath = join(__dirname, '../../react-app/src/utils/types');
   const contractsList = readdirSync(hardhatContractsPath).filter((path) => /\.sol$/.test(path));
@@ -12,18 +12,22 @@ async function shareFiles() {
     const contractSlug = slug.replace(/\.sol/, '');
     const contractJSON = contractSlug + '.json';
     const contractJSONPath = join(reactContractsPath, contractJSON);
-    const contractType = contractSlug + '.d.ts';
+    const contractType = contractSlug + '.ts';
     const contractTypePath = join(reactContractsTypesPath, contractType);
+    const contractTypeCommon = 'common.ts';
+    const contractTypeCommonPath = join(reactContractsTypesPath, contractTypeCommon);
     const artifact = artifacts.readArtifactSync(contractSlug);
     if (!existsSync(reactContractsPath)) {
-      mkdirSync(reactContractsPath);
+      mkdirSync(reactContractsPath, { recursive: true });
     }
     writeFileSync(contractJSONPath, JSON.stringify(artifact, null, 2));
     const typeData = readFileSync(join(hardhatContractsTypePath, contractType), 'utf8');
+    const typeCommonData = readFileSync(join(hardhatContractsTypePath, contractTypeCommon), 'utf8');
     if (!existsSync(reactContractsTypesPath)) {
-      mkdirSync(reactContractsTypesPath);
+      mkdirSync(reactContractsTypesPath, { recursive: true });
     }
     writeFileSync(contractTypePath, typeData);
+    writeFileSync(contractTypeCommonPath, typeCommonData);
   });
   return {
     contractsNum: contractsList.length,
@@ -36,6 +40,7 @@ shareFiles()
   .then(({ contractsNum, reactContractsPath, reactContractsTypesPath }) => {
     console.log(`  ✓ ${contractsNum} contract(s) copied to ${reactContractsPath}`);
     console.log(`  ✓ ${contractsNum} contract(s) type(s) copied to ${reactContractsTypesPath}`);
+    console.log(`  ✓ Contract type required module common.ts copied to ${reactContractsTypesPath}`);
     process.exit(0);
   })
   .catch((error) => {
