6485c173a75353c564410edaf7e98e2e2fcdd971
Alberto <25537372+albertobas@users.noreply.github.com>
2023-12-03 10:36:20 +0100

    fix: export constant

--- react-app/src/utils/helpers.ts
+++ react-app/src/utils/helpers.ts
@@ -5,17 +5,17 @@ import { MintableERC20 } from 'utils/types/MintableERC20';
 
 const addresses = require('./contracts/contracts-addresses.json');
 
-const isEthereum = typeof window.ethereum !== 'undefined';
+export const isEthereum = typeof window.ethereum !== 'undefined';
 
 export const fetchData = async () => {
   const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
 
-  const provider = new ethers.providers.JsonRpcProvider(); //window.ethereum);
-  const contract = new ethers.Contract(addresses['MintableERC20'], mintableErc20.abi, provider); // as MintableERC20;
+  const provider = new ethers.providers.JsonRpcProvider();
+  const contract = new ethers.Contract(addresses['MintableERC20'], mintableErc20.abi, provider);
 
   const web3Modal = new Web3Modal();
   const connection = await web3Modal.connect();
-  const userProvider = new ethers.providers.Web3Provider(connection); //window.ethereum);
+  const userProvider = new ethers.providers.Web3Provider(connection);
   const signer = userProvider.getSigner();
   const signerContract = new ethers.Contract(addresses['MintableERC20'], mintableErc20.abi, signer) as MintableERC20;
 
